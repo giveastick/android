c765f14afa32b1bfe313aa368796437b6ba7a923
Maxime Lebastard <lebastardmaxime@gmail.com>
2014-02-14 14:27:09 +0100

    Modifs webservice json

--- src/com/iia/giveastick/view/giveastick/UserLoginActivity.java
+++ src/com/iia/giveastick/view/giveastick/UserLoginActivity.java
@@ -91,12 +91,14 @@ public class UserLoginActivity extends HarmonyFragmentActivity {
 				JSONObject postData = new JSONObject();
 				postData.put("email", mEmail.getText().toString());
 				postData.put("password", mPassword.getText().toString());
-				JSONObject jsonResult = new GetWsJson("/login", postData, Verb.POST).getJSONObject();
 				
-				if(!GetWsJson.treatError(UserLoginActivity.this, jsonResult))
-				{
-					result = ParserUser.getUser(jsonResult, UserLoginActivity.this);
-				}
+				//JSONObject jsonResult = new GetWsJson("/debug", postData, Verb.POST).getJSONObject();	
+			    String jsonResult = new GetWsJson("/login", postData, Verb.POST).getString();	
+			    Log.d(TAG, jsonResult);
+				//if(!GetWsJson.treatError(UserLoginActivity.this, jsonResult))
+				//{
+				//	result = ParserUser.getUser(jsonResult, UserLoginActivity.this);
+				//}
 				
 			}
 			catch(Exception e)
